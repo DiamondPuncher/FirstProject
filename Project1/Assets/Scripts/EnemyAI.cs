da7b326747b1913c2a1926f98f0735deb82a4882
Zechoz <zacharymunn07@gmail.com>
2024-09-13 09:45:54 -0700

    updated AI and enemy Sprites

--- Project1/Assets/Scripts/EnemyAI.cs
+++ Project1/Assets/Scripts/EnemyAI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,11 +11,15 @@ public class EnemyAI : MonoBehaviour
     float chaseSpeed = 10f;
     [SerializeField]
     float chaseTriggerDistance = 5.0f;
+    [SerializeField]
+    Boolean returnHome = true;
+    Vector3 home;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        home = transform.position;
     }
 
     // Update is called once per frame
@@ -23,6 +28,7 @@ public class EnemyAI : MonoBehaviour
         //if the player gets too close
         Vector3 playerPosition = player.transform.position;
         Vector3 chaseDir = playerPosition - transform.position;
+        Vector3 homeDir = home - transform.position;
         if (chaseDir.magnitude < chaseTriggerDistance)
         {
             //chase the player
@@ -31,9 +37,15 @@ public class EnemyAI : MonoBehaviour
             chaseDir.Normalize();
             GetComponent<Rigidbody2D>().velocity = chaseDir * chaseSpeed;
         }
+        else if(returnHome && homeDir.magnitude > 0.2f)
+        {
+            //return home
+            homeDir.Normalize();
+            GetComponent<Rigidbody2D>().velocity = homeDir * chaseSpeed;
+        }
         else
         {
-            //if the player is NOT close, stop moving
+            //if the player is NOT close & and we're not trying to return home, stop moving
             GetComponent<Rigidbody2D>().velocity = Vector3.zero;
         }
 
